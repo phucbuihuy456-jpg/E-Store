8ca2cfa650967949c23635c324aa6d3f016714d8
PhucBH <phucbuihuy456@gmail.com>
2026-09-11 14:18:56 +0700

    Connect to Database

---
+++ E-Store/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace E_Store.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string FullName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+
+    public string Role { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public virtual Cart? Cart { get; set; }
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+
+    public virtual ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
+}
